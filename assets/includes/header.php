2344affffb5562bf87db31ea1a69bc65e5004138
liannefm <lfmeiresonne@gmail.com>
2026-01-14 11:26:49 +0100

    includes toegevoegd

---
+++ assets/includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://kit.fontawesome.com/0f7734b39c.js" crossorigin="anonymous"></script>
+
+    <link rel="icon" href="assets/favicon/favicon.webp" type="image/x-icon">
+
+    <link rel="icon" type="assets/favicon/favicon.webp" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="assets/favicon/favicon.webp" sizes="32x32" href="favicon-32x32.png">
+
+    <title>Vuurmeisje en Watermeisje</title>
+    <link rel="stylesheet" href="style.css">
+</head>
--- index.php
+++ index.php
@@ -1,22 +1,9 @@
-<!DOCTYPE html>
-<html lang="nl">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <script src="https://kit.fontawesome.com/0f7734b39c.js" crossorigin="anonymous"></script>
-
-    <link rel="icon" href="assets/favicon/favicon.webp" type="image/x-icon">
-
-    <link rel="icon" type="assets/favicon/favicon.webp" sizes="32x32" href="favicon-32x32.png">
-    <link rel="icon" type="assets/favicon/favicon.webp" sizes="32x32" href="favicon-32x32.png">
-
-    <title>Vuurmeisje en Watermeisje</title>
-    <link rel="stylesheet" href="style.css">
-</head>
+<?php
+include('assets/includes/header.php');
+?>
 
 <body>
+  <div id="bodyhomepagina">
     <div id="achtergrond">
         <div id="titelboxboven">
             <h1 id="fireboy">Firegirl</h1>
@@ -36,6 +23,7 @@
         ?>
 
     </div>
+  </div>
 </body>
 
 </html>
