afb283d5336920d11ef6ab7cd7c4f68228426f79
liannefm <lfmeiresonne@gmail.com>
2026-01-16 10:53:10 +0100

    levelknoppen styling

--- levelscherm.php
+++ levelscherm.php
@@ -4,14 +4,27 @@ include('assets/includes/header.php');
 
 <body>
     <div id="keuzelevelachtergrond">
-        <div id="backbuttonbox">
-            <a href="index.php" id="backbutton"><i class="fa-solid fa-arrow-left"></i></a>
+        <div id="bovenbalk">
+            <div id="backbuttonbox">
+                <a href="index.php" id="backbutton"><i class="fa-solid fa-arrow-left"></i></a>
+            </div>
+
+            <div id="winterwonderland">Winter Wonderland</div>
         </div>
 
-        <div id="winterwonderland">Winter Wonderland</div>
+        <div id="levelkeuze">
+            <a href="level-3.php" class="level extra"></a>
+            <a href="level-2.php" class="level extra"></a>
+            <a href="level-1.php" class="level main"></a>
+        </div>
+
+        <!-- <div id="level1button">
+            <a href="level-1.php" id="levelbutton"><img src="assets/img/levelknop.png"></a>
+        </div> -->
 
     </div>
     <div id="lichtekeuzelevelachtergrond">
+
         <?php
         include('assets/includes/settings.php');
         ?>
